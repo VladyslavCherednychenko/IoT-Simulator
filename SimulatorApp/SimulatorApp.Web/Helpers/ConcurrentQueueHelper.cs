d3972a980f76ec20ee75dd74c4564da0b047f8e9
Vladyslav Cherednychenko <vladislav.cherednychenko@gmail.com>
2026-02-15 16:40:35 +0100

    feat: add MQTT listener to Blazor project

---
+++ SimulatorApp/SimulatorApp.Web/Helpers/ConcurrentQueueHelper.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Concurrent;
+
+namespace SimulatorApp.Web.Helpers;
+
+public static class ConcurrentQueueHelper
+{
+    public static List<T> DrainQueue<T>(ConcurrentQueue<T> queue)
+    {
+        var items = new List<T>();
+        while (queue.TryDequeue(out var item))
+        {
+            items.Add(item);
+        }
+        return items;
+    }
+}
--- SimulatorApp/SimulatorApp.Web/Program.cs
+++ SimulatorApp/SimulatorApp.Web/Program.cs
@@ -3,6 +3,7 @@ using SimulatorApp.Core.Enums;
 using SimulatorApp.Core.Models;
 using SimulatorApp.Infrastructure.Data;
 using SimulatorApp.Web.Components;
+using SimulatorApp.Web.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,11 +13,11 @@ builder.Services.AddRazorComponents()
 builder.Services.AddDbContext<AppDbContext>(options =>
 {
     options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection"),
-        npgsqlOptionsAction: npgsqlOptions =>
-        {
-            npgsqlOptions.EnableRetryOnFailure();
-        });
+        npgsqlOptionsAction: npgsqlOptions => npgsqlOptions.EnableRetryOnFailure());
 });
+
+builder.Services.AddSingleton<MqttListenerService>();
+builder.Services.AddHostedService(sp => sp.GetRequiredService<MqttListenerService>());
 
 var app = builder.Build();
 
@@ -27,9 +28,9 @@ using (var scope = app.Services.CreateScope())
 
     if (!await db.Devices.AnyAsync())
     {
-        var dev0 = new Device { MAC = "F3D24C8D0EA5", Model = "Climate Sensor x1402abc", Location = "livingroom", IsOnline = true };
-        var dev1 = new Device { MAC = "56699D64012D", Model = "Motion Sensor xyz123", Location = "backyard", IsOnline = true };
-        var dev2 = new Device { MAC = "3A2CE01FE222", Model = "Unusual Sensor t900", Location = "secret room", IsOnline = true };
+        var dev0 = new Device { MAC = "F3D24C8D0EA5", Model = "Climate Sensor x1402abc", Location = "livingroom", IsOnline = true, LastSeen = DateTime.UtcNow };
+        var dev1 = new Device { MAC = "56699D64012D", Model = "Motion Sensor xyz123", Location = "backyard", IsOnline = true, LastSeen = DateTime.UtcNow };
+        var dev2 = new Device { MAC = "3A2CE01FE222", Model = "Unusual Sensor t900", Location = "secret room", IsOnline = true, LastSeen = DateTime.UtcNow };
 
         await db.Devices.AddRangeAsync(
             dev0,
@@ -38,13 +39,13 @@ using (var scope = app.Services.CreateScope())
         );
 
         await db.Sensors.AddRangeAsync(
-            new Sensor { Device = dev0, SensorType = SensorType.Temperature, Name = "Temperature Sensor", IsOnline = true },
-            new Sensor { Device = dev0, SensorType = SensorType.Humidity, Name = "Humidity Sensor", IsOnline = true },
-            new Sensor { Device = dev0, SensorType = SensorType.SoC, Name = "Battery Level Sensor", IsOnline = true },
-            new Sensor { Device = dev1, SensorType = SensorType.Motion, Name = "Motion Sensor", IsOnline = true },
-            new Sensor { Device = dev2, SensorType = SensorType.Resistance, Name = "Resistance Sensor", IsOnline = true },
-            new Sensor { Device = dev2, SensorType = SensorType.Pressure, Name = "Pressure Sensor", IsOnline = true },
-            new Sensor { Device = dev2, SensorType = SensorType.CO2, Name = "CO2 Sensor", IsOnline = true }
+            new Sensor { Device = dev0, SensorType = SensorType.Temperature, Name = "Temperature Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev0, SensorType = SensorType.Humidity, Name = "Humidity Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev0, SensorType = SensorType.SoC, Name = "Battery Level Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev1, SensorType = SensorType.Motion, Name = "Motion Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev2, SensorType = SensorType.Resistance, Name = "Resistance Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev2, SensorType = SensorType.Pressure, Name = "Pressure Sensor", IsOnline = true, LastSeen = DateTime.UtcNow },
+            new Sensor { Device = dev2, SensorType = SensorType.CO2, Name = "CO2 Sensor", IsOnline = true, LastSeen = DateTime.UtcNow }
         );
 
         await db.SaveChangesAsync();
